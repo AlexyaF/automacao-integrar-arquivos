27e32dc00e74c66a103b4d75fa9af702e959a017
Allexya Fortunato <allexya.fortunato@hotmail.com>
2024-12-18 08:53:12 -0300

    Adicionando tratativa se caso erro de timeout para testes unitários

--- integracaoManual.py
+++ integracaoManual.py
@@ -6,11 +6,14 @@ from ftplib import FTP
 from selenium.webdriver.chrome.service import Service 
 import pandas as pd
 import os
+
 from dotenv import load_dotenv
+import time
 
 #Carregando palavras chaves
 load_dotenv()
 
+returns = []
 
 servico = Service(ChromeDriverManager().install()) # Identifica a versão do navegador atual e vai baixar o Chrome Driver mais recente.
 navegador = webdriver.Chrome()
@@ -18,7 +21,22 @@ navegador.set_page_load_timeout(10000)
 
 funcoes.abrir_driver(navegador)
 
-retorno = funcoes.integrar(navegador)
+try:
+    retorno = funcoes.integrar(navegador, returns)
+
+except TimeoutException:
+    funcoes.marcacao_cod('Timeout na tentativa. Aguardando 5 minutos antes de tentar novamente.', 'erro')
+    navegador.switch_to.default_content()  # Sai do iframe
+    time.sleep(300)  # Espera 5 minutos antes da nova tentativa
+    retorno = funcoes.integrar(navegador, returns)
+
+
+except Exception as e:
+    funcoes.marcacao_cod(f"Erro inesperado: {e}. Aguardando 5 minutos antes de tentar novamente.", 'erro')
+    navegador.switch_to.default_content()  # Sai do iframe
+    time.sleep(300)  # Espera 5 minutos antes da nova tentativa
+    retorno = funcoes.integrar(navegador, returns)
+
 
 #Criar Excel com resultado
 pathSalvar = os.getenv('PATHSAVE')
