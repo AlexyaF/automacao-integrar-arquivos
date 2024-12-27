25f8ac0c452de24aa7700d7b859d4330bdd0c767
Allexya Fortunato <allexya.fortunato@hotmail.com>
2024-12-27 10:51:02 -0300

    Ajuste funções\n-Ajuste na função de verificação dos arquivos: tratativa de linhas fora do layout esperado.\n-Criação da função email_erro: somente para sinalizar erros ao decorrer do código.\n-Ajuste na função integrar: tratativa de erro timeout.

--- funcoes.py
+++ funcoes.py
@@ -6,11 +6,11 @@ import shutil
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, WebDriverException
 import time
 from time import sleep
 from dotenv import load_dotenv
-from dotenv import load_dotenv
+
 
 
 load_dotenv()
@@ -44,7 +44,7 @@ def conexao_ftp():
         marcacao_cod('Conexão estabelecida com FTP')
     except Exception as e:
         marcacao_cod(f"Erro ao conectar ao FTP: {e}", 'erro')
-    
+        email_erro('FTP', e)
     return ftp
 
 
@@ -52,17 +52,43 @@ def conexao_ftp():
 def verifArquivos():
     marcacao_cod("Verificação dos arquivos")
     path = os.getenv("PATHFOLDER")
+    # Listando os arquivos da pasta
     files = os.listdir(path)
 
     for file in files:
         arquivo = os.path.join(path, file)
-        # Abrindo o arquivo para procurar tabulações
-        with open(arquivo, 'r', encoding='utf-8') as arq:
-            linhas_corrigidas = [linha.replace('\t', '') for linha in arq]
 
-        # Salvando o arquivo corrigido
+        with open(arquivo, 'r', encoding='utf-8') as arq:
+            linhas = arq.readlines()
+
+        if not linhas:
+            marcacao_cod(f"Aviso: O arquivo {arquivo} está vazio e será ignorado.", 'erro')
+            continue
+
+        # Padrão do tamanho (150 manual)
+        padrao_tamanho = 150
+
+        # Realiza as validações na memória
+        linhas_corrigidas = []
+        for linha in linhas:
+            # Remover tabulações
+            linha = linha.replace('\t', '')
+
+            # Ajustar linhas fora do padrão
+            if len(linha.strip()) != padrao_tamanho:
+                print(f"{file} fora do padrão esperado")
+                if len(linha) > 31 and linha[31] != " ":
+                    linha = linha[:30] + " " + linha[30:]
+                    print(f"Arquivo corrigido: {arquivo}")
+            
+            linhas_corrigidas.append(linha)
+        
+
+        # Sobrescreve o arquivo com as linhas corrigidas
         with open(arquivo, 'w', encoding='utf-8') as arq:
             arq.writelines(linhas_corrigidas)
+
+        
 
 
 
@@ -92,7 +118,34 @@ def enviar_email_com_anexo(anexo=None):
         marcacao_cod(f"Email enviado para {destinatario} ", 'log')
     except Exception as e:
         marcacao_cod(f"Erro ao enviar o email: {e}", 'erro')
+        email_erro('EMAIL', e)
   
+
+def email_erro(file, erro):
+    marcacao_cod("Mandando E-mail de erro")
+    data = datetime.today()
+    data_atual = data.strftime("%d/%m/%Y")
+    destinatario = os.getenv("DESTINATARIO")
+    copia = os.getenv("COPIA")
+    if copia:
+    # Removendo os colchetes e convertendo para lista
+        copia = copia.strip("[]").replace(" ", "").split(",")
+    else:
+        copia = []
+
+    assunto = f"****TESTE**** Integração CPFL ERRO!!! - {data_atual}"
+    corpo  = f"Segue arquivo e erro abaixo.\n {file} - Erro: {erro}"
+
+    try:
+        outlook = win32.Dispatch('Outlook.Application')
+        email = outlook.CreateItem(0)
+        email.To = destinatario
+        email.CC = ";".join(copia) if copia else ""
+        email.Subject = assunto
+        email.Body = corpo
+        email.Send()
+    except Exception as e:
+        marcacao_cod(f"Erro ao enviar o email: {e}", 'erro')
 
 
 def mover_arquivos_processado(folder, ftp):
@@ -128,6 +181,7 @@ def mover_arquivos_processado(folder, ftp):
 
         except Exception as e:
             marcacao_cod(f"Erro ao conectar ao FTP: {e}", 'erro')
+            email_erro(f'Erro ao conectar na pasta {folder}', e)
 
 
 def mover_integrados(file, allpath):
@@ -188,8 +242,9 @@ def abrir_driver(navegador):
         wait = WebDriverWait(navegador, 10000)  # Substitua 'navegador' pelo nome do driver
         element = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="field5-suggestions"]//span[text()="RETORNO CPFL"]')))
         element.click()
-    except:
+    except Exception as e:
         marcacao_cod("ERRO!", 'erro')
+        email_erro("Abrir navegador", e)
 
 
 
@@ -208,6 +263,7 @@ def mover_arquivos_txt(folder, ftp):
         ftp.cwd(f"/cobrconta/GMP/{folder}/0713464419/RetornoGMP")
     except Exception as e:
         marcacao_cod(f"Erro ao conectar ao FTP: {e}", 'erro')
+        email_erro(f"Erro ao conectar ftp {folder}", e)
         return  # Interrompe a execução em caso de erro
 
     try:
@@ -242,8 +298,10 @@ def mover_arquivos_txt(folder, ftp):
 
     except UnicodeDecodeError as e:
         marcacao_cod(f"Erro de decodificação: {e}", 'erro')
+        email_erro('Erro decodificação', e)
     except Exception as e:
         marcacao_cod(f"Erro ao listar ou processar arquivos: {e}", 'erro')
+        email_erro('Erro Erro ao listar ou processar arquivos', e)
 
 
 
@@ -251,11 +309,16 @@ def mover_arquivos_txt(folder, ftp):
 def integrar(navegador, returns):
     #função que localiza o retorno indicando o fim da integração
     def localizar_retorno():
-        retorno_element = WebDriverWait(navegador, 10000).until(
+        try:
+            retorno_element = WebDriverWait(navegador, 10000).until(
             EC.visibility_of_element_located((By.XPATH, '//*[@id="Label9"]'))
-        )
-        texto = retorno_element.text
-        return texto
+            )
+            texto = retorno_element.text
+            return texto
+        except TimeoutException as e:
+            marcacao_cod("Timeout ao localizar retorno.", "erro")
+            email_erro("Erro ao buiscar retorno", e )
+            raise  # Repassa o erro para que o `try` principal lide com ele
         
     # Logando tempo de execução
     start_time = time.time()
@@ -267,55 +330,65 @@ def integrar(navegador, returns):
     for file in files:
         file_start_time = time.time()
         allpath = os.path.join(pathFolder, file)
-        texto = None  # Para armazenar o status do processamento
-        
-        #Upload arquivo
-        wait = WebDriverWait(navegador, 10000) # Aguarda até o elemento estar visível
-        navegador.switch_to.frame("cont") # mudando para o iframe
-        input_element = wait.until(EC.visibility_of_element_located((By.ID, 'Upload1'))) #aguardo até a visibilidade do elemento e identifico 
-        input_element.send_keys(allpath) #envio o arquivo
-
-        #Integrar click
-        navegador.find_element('xpath','//*[@id="Btncef"]').click()
-
-
-        
+        texto = None
         try:
+            # Upload arquivo
+            navegador.switch_to.frame("cont")
+            wait = WebDriverWait(navegador, 10000)
+            input_element = wait.until(EC.visibility_of_element_located((By.ID, 'Upload1')))
+            input_element.send_keys(allpath)
+            navegador.find_element('xpath', '//*[@id="Btncef"]').click()
+
+            # Localizar retorno
             texto = localizar_retorno()
+
+            # Mover arquivo integrado
+            mover_integrados(file, allpath)
+            
+        except TimeoutException:
+            marcacao_cod(f"Timeout ao processar arquivo {file}. Tentando novamente...", "erro")
+            sleep(160)  # Aguarda antes de tentar novamente
+            navegador.switch_to.default_content()
             try:
+                texto = localizar_retorno()
                 mover_integrados(file, allpath)
             except Exception as e:
-                marcacao_cod(f"Erro ao mover arquivo {file}: {e}", 'erro')
+                marcacao_cod(f"Erro ao localizar retorno do arquivo {file}: {e}", "erro")
+                texto = f"Erro de retorno"
+                email_erro(file, e)
 
-        except TimeoutException:
-            # Captura exclusivamente erros de tempo limite
-            marcacao_cod("Tempo limite excedido ao esperar pela integração.", 'erro')
-            navegador.switch_to.default_content()  # Sai do iframe
+        except WebDriverException as e:
+            marcacao_cod(f"Erro no Selenium ao processar o arquivo {file}: {e}. Tentando novamente...", "erro")
+            sleep(160)  # Aguarda antes de tentar novamente
             try:
-                sleep(250)
-                marcacao_cod("TENTANDO NOVAMENTE APÓS 4 MINUTOS")
                 texto = localizar_retorno()
-                try:
-                    mover_integrados(file, allpath)
-                except Exception as e:
-                    marcacao_cod(f"Erro ao mover arquivo {file}: {e}", 'erro')
-                    
-            except TimeoutException:
-                 marcacao_cod("Erro de timeout", 'erro')
-                 texto = "Erro: tempo limite excedido."     
+                mover_integrados(file, allpath)
+            except Exception as e:
+                marcacao_cod(f"Erro ao localizar retorno do arquivo {file}: {e}", "erro")
+                texto = f"Erro de retorno"
+                email_erro(file, e)
+
 
         except Exception as e:
-            # Captura erros gerais
-            marcacao_cod(f"Erro inesperado ao processar o arquivo {file}: {e}", 'erro')
-            texto = f"Erro inesperado: {e}"
+            marcacao_cod(f"Erro inesperado ao processar o arquivo {file}: {e}. Tentando novamente...", "erro")
+            sleep(160)  # Aguarda antes de tentar novamente
+            try:
+                texto = localizar_retorno()
+                mover_integrados(file, allpath)
+            except Exception as e:
+                marcacao_cod(f"Erro ao localizar retorno do arquivo {file}: {e}", "erro")
+                texto = f"Erro de retorno"
+                email_erro(file, e)
+            
         finally:
-            navegador.switch_to.default_content()  # Sai do iframe   
-        
-        elapsed_time = time.time() - file_start_time  # Fim do tempo
-        marcacao_cod(f"CONCLUÍDO PARA: {file}. Tempo: {elapsed_time:.2f} segundos")
+            navegador.switch_to.default_content()
+
+        elapsed_time = time.time() - file_start_time
+        marcacao_cod(f"Concluído para: {file}. Tempo: {elapsed_time:.2f} segundos")
 
         joinArquivoRetorno = [file, texto]
         returns.append(joinArquivoRetorno)
+        
     marcacao_cod(f"PROCESSAMENTO TOTAL FINALIZADO. Tempo total: {time.time() - start_time:.2f} segundos", "titulo")
     return returns
 
